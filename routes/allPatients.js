e36dcbabcc2f374aae175f7821b477993414c9e3
Mennatallah Hamdy <menna.h.m.elsayed@gmail.com>
2021-07-09 18:02:44 +0200

    all patients

--- routes/allPatients.js
+++ routes/allPatients.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const knex = require('../db/config');
-
+var pa;
 /* GET ALL . */
 router.get('/allPatients', function (req, res) {
 
@@ -11,8 +11,8 @@ router.get('/allPatients', function (req, res) {
         knex.raw('GROUP_CONCAT(??.??) AS ?? ',
         ['medications','product_name',"medications"]),'medical_history.*')
             .from('patients')
-            .join('medications', 'patients.patient_id', '=', 'medications.patient_id')
-        .join('medical_history', 'patients.patient_id', '=', 'medical_history.patient_id').groupBy('patients.patient_id')
+            .leftOuterJoin('medications', 'patients.patient_id', '=', 'medications.patient_id')
+        .leftOuterJoin('medical_history', 'patients.patient_id', '=', 'medical_history.patient_id').groupBy('patients.patient_id')
         .then((patients) => {
             if (patients.length) {
 
@@ -26,5 +26,57 @@ router.get('/allPatients', function (req, res) {
         .catch((err) => { res.status(500).send('server error please come back later'); throw err })
 
 });
+router.get('/allPatients/:id', function (req, res) {
+    const id = req.params.id;
+    console.log(id);
+
+    knex.select('patients.*',
+        knex.raw('GROUP_CONCAT(??.??) AS ?? ',
+            ['medications', 'product_name', "medications"]),
+        'medical_history.*')
+        .from('patients')
+        .leftOuterJoin('medications', 'patients.patient_id', '=', 'medications.patient_id')
+        .leftOuterJoin('medical_history', 'patients.patient_id', '=', 'medical_history.patient_id')
+        .innerJoin('visit', 'patients.patient_id', '=', 'visit.patient_id').where('visit.doctor_id', id)
+        .groupBy('patients.patient_id')
+        .then((patients) => {
+            if (patients.length) {
+                pa = patients;
+//                res.json(patients);
+
+                console.log(patients);
+            } else {
+                res.json([])
+            }
+        })
+
+        .catch((err) => {
+            res.status(500).send('server error please come back later');
+            throw err
+        });
+
+    knex.select('patient_id',knex.raw('GROUP_CONCAT(??) AS ?? ',
+        [ 'date', "visits"]))
+        .from('visit')
+        .where('doctor_id', id).andWhere(function () {
+        this.where('date', '<', (new Date()))
+    })
+        .groupBy('patient_id')
+        .then((patients) => {
+            if (patients.length) {
+                //var pa = patients;
+                res.json({patient:pa, visit: patients});
+
+                console.log( patients);
+            } else {
+                res.json([])
+            }
+        })
+        .catch((err) => {
+            res.status(500).send('server error please come back later');
+            throw err
+        });
+});
+
 
 module.exports = router;
