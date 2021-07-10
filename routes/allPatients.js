b9ced47c306ac2ec7863830b88f94157b0257fc9
Mennatallah Hamdy <menna.h.m.elsayed@gmail.com>
2021-07-10 11:01:57 +0200

    recheck

--- routes/allPatients.js
+++ routes/allPatients.js
@@ -4,34 +4,9 @@ const knex = require('../db/config');
 var pa;
 const authenticateToken = require("../middleware/checkAuth");
 
-/* GET ALL . */
-// router.get('/allPatients', function (req, res) {
-//
-//     console.log("here");
-//
-//     knex.select('patients.*',
-//         knex.raw('GROUP_CONCAT(??.??) AS ?? ',
-//         ['medications','product_name',"medications"]),'medical_history.*')
-//             .from('patients')
-//             .leftOuterJoin('medications', 'patients.patient_id', '=', 'medications.patient_id')
-//         .leftOuterJoin('medical_history', 'patients.patient_id', '=', 'medical_history.patient_id').groupBy('patients.patient_id')
-//         .then((patients) => {
-//             if (patients.length) {
-//
-//                 res.json(patients);
-//
-//                 console.log(patients);
-//             } else {
-//                 res.json([])
-//             }
-//         })
-//         .catch((err) => { res.status(500).send('server error please come back later'); throw err })
-//
-// });
 router.get('/allPatients/:id',authenticateToken, function (req, res) {
     const doctor_id = req.user.doctor_id;
     const id = req.params.id;
-
     if(id ==='1'){
     knex.select('patients.*',
         knex.raw('GROUP_CONCAT(??.??) AS ?? ',
@@ -45,9 +20,7 @@ router.get('/allPatients/:id',authenticateToken, function (req, res) {
         .then((patients) => {
             if (patients.length) {
                 pa = patients;
-//                res.json(patients);
 
-                console.log(patients);
             } else {
                 res.json([])
             }
@@ -66,10 +39,8 @@ router.get('/allPatients/:id',authenticateToken, function (req, res) {
         .groupBy('patient_id')
         .then((patients) => {
             if (patients.length) {
-                //var pa = patients;
                 res.json({patient:pa, visit: patients});
 
-                console.log( patients);
             } else {
                 res.json([])
             }
@@ -95,7 +66,7 @@ router.get('/allPatients/:id',authenticateToken, function (req, res) {
 
                 res.json(patients);
 
-                console.log(patients);
+
             } else {
                 res.json([])
             }
