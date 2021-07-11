4983aa9ffd73bfdc7a7f9aa567ffd6e9ad4fb564
fady tadrous <fadytadrous.ft@gmail.com>
2021-07-11 12:56:02 +0200

    Update scheduleVisit.js

--- routes/scheduleVisit.js
+++ routes/scheduleVisit.js
@@ -6,15 +6,48 @@ const db = require('../db/config');
 router.post('/submitBooking', (req, res) => {
     console.log(req.body.name);
     console.log(req.body.patientSSN);
+
     const data = req.body;
     console.log(data);
-    res.send(data);
-    db('visit')
-        .insert({ patient_id: 123, doctor_id: 434, clinic_id: 32, secretary_id: 54, visit_id: 12 }, { includeTriggerModifications: true })
+
+    const patientSSN = req.body.patientSSN;
+    const doctor = req.body.name;
+    const doctorNameArr = doctor.split(" ");
+    const visitDate = req.body.day;
+    const visitTime = req.body.time;
+    db('patients').where('ssn', patientSSN)
+        .then((res) => {
+            patientId = res[0].patient_id;
+            console.log(patientId);
+        })
+        .catch(err => console.log(err))
+        .then(() => {
+            db('doctors').where('first_name', doctorNameArr[0]).andWhere('last_name', doctorNameArr[1])
+                .then((res) => {
+                    doctorId = res[0].doctor_id;
+                    console.log(doctorId);
+                })
+                .catch(err => console.log(err))
+                .then(() => {
+                    db('visit')
+                        .insert({
+                            patient_id: patientId, doctor_id: doctorId,
+                            clinic_id: 15, secretary_id: 25, date: visitDate, time: visitTime
+                        }, { includeTriggerModifications: true })
+                        .then((res) => {
+                            console.log(res);
+                        })
+                        .catch(err => { console.log(err) })
+
+                })
+
+        });
+
+
 
 })
 router.get('/submitBooking', (req, res) => {
     res.send("API Test");
 })
 
-module.exports = router;  
+module.exports = router;
